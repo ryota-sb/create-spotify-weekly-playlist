d86527676fb0fc15d73717739ded44ff5500ad5d
ryota-sb <ryota.sb.1206@gmail.com>
2020-11-21 22:17:56 +0900

    layout 修正

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,6 @@
   <v-app dark>
     <v-navigation-drawer
       v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
       app
     >
       <v-list>
@@ -12,8 +9,6 @@
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
-          router
-          exact
         >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
@@ -25,37 +20,10 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -83,7 +51,7 @@
       :absolute="!fixed"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>&copy; {{ new Date().getFullYear() }} Ryota</span>
     </v-footer>
   </v-app>
 </template>
@@ -92,25 +60,20 @@
 export default {
   data () {
     return {
-      clipped: false,
+      title: 'Create Spotify Playlist',
       drawer: false,
-      fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home',
+          title: 'HOME',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          icon: 'mdi-information-outline',
+          title: 'ABOUT',
+          to: '/about'
         }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      ]
     }
   }
 }
---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <h1>ABOUT PAGE</h1>
+      <p>Spotifyで毎週自動生成されるプレイリストをスプレッドシートから取得するためだけのアプリ</p>
+    </v-col>
+  </v-row>
+</template>
